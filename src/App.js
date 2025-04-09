7b695c39eda5bae157484da4891343717e62e718
hw <lhy3899@gmail.com>
2025-04-10 00:04:54 +0900

    Footer 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Banner from './components/Banner';
 import Nav from './components/Nav';
 import Row from './components/Row';
 import requests from './api/requests';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -44,7 +45,9 @@ function App() {
         id="DM"
         fetchUrl={requests.fetchDocumentaries}
       />
-      
+
+
+      <Footer />
     </div>
   );
 }
