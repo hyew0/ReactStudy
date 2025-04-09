b87f9e932a767efdfc469904be147f999661a2ba
hw <lhy3899@gmail.com>
2025-04-10 00:02:17 +0900

    화살표 보이도록 수정, 화살표 동작되도록 추가

--- src/components/Row.js
+++ src/components/Row.js
@@ -20,23 +20,40 @@ const Row =({title, fetchUrl, isLargeRow, id}) => {
         <section className="row">
             <h2>{title}</h2>
             <div className="slider">
-                <div className="slider__arrow-left">
+                <div 
+                    className="slider__arrow-left"
+                    onClick={() => {
+                        document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+                    }}
+                >
                     <span className="arrow">
                         {"<"}
                     </span>
                 </div>
-            </div>
 
-            <div id={id} className="row__posters">
-                {movies.map((movie) => (
-                    <img 
+                <div id={id} className="row__posters">
+                    {movies.map((movie) => (
+                        <img 
                         key = {movie.id}
                         className={`row__poster ${isLargeRow && "row__posterLarge"}`}
                         src={`${BASE_URL}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
                         loading="lazy"
                         alt={movie.name}
-                    />
-                ))}
+                        />
+                    ))}
+                </div>
+
+                <div 
+                    className="slider__arrow-right"
+                    onClick={() => {
+                        document.getElementById(id).scrollLeft += window.innerWidth - 80;
+                    }}
+                >
+                    <span className="arrow">
+                        {">"}
+                    </span>
+                </div>
+
             </div>
         </section>
     )
