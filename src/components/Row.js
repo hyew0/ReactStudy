80695d793e94928aad29901de44dff7735b8efce
hw <lhy3899@gmail.com>
2025-04-10 09:27:50 +0900

    modal 기초 추가

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import axios from '../api/axios'
 import './Row.css'
+import MovieModal from './MovieModal'
 
 const Row =({title, fetchUrl, isLargeRow, id}) => {
     const [movies, setMovies] = useState([]);
+
+    /* movie modal 부분 */
+    const [modalOpen, setModalOpen] = useState(false);
+    const [movieSelected, setMovieSelected] = useState({});
+
     const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
     useEffect( () => {
@@ -15,6 +21,12 @@ const Row =({title, fetchUrl, isLargeRow, id}) => {
         setMovies(request.data.results);
         return request;
     }
+
+    const handleClick = (movie) => {
+        setModalOpen(true);
+        setMovieSelected(movie);
+    }
+
 
     return (
         <section className="row">
@@ -34,11 +46,12 @@ const Row =({title, fetchUrl, isLargeRow, id}) => {
                 <div id={id} className="row__posters">
                     {movies.map((movie) => (
                         <img 
-                        key = {movie.id}
-                        className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-                        src={`${BASE_URL}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
-                        loading="lazy"
-                        alt={movie.name}
+                            key = {movie.id}
+                            onClick={() => handleClick(movie)}
+                            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                            src={`${BASE_URL}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                            loading="lazy"
+                            alt={movie.name}
                         />
                     ))}
                 </div>
@@ -55,6 +68,15 @@ const Row =({title, fetchUrl, isLargeRow, id}) => {
                 </div>
 
             </div>
+
+            {
+                modalOpen && (
+                    <MovieModal 
+                    {...movieSelected}
+                        setModalOpen={setModalOpen}
+                    />
+                )
+            }
         </section>
     )
 }
