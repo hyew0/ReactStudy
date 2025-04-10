847f057ef1e12e2c5df2294bd6851700630d4a5f
hw <lhy3899@gmail.com>
2025-04-10 14:26:14 +0900

    useParams를 이용한 상세페이지 구현

--- src/Pages/SearchPage/index.js
+++ src/Pages/SearchPage/index.js
@@ -1,11 +1,14 @@
 import axios from '../../api/axios';
 import React, { useEffect } from 'react'
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import './SearchPage.css'
 import { useDebounce } from '../../hooks/useDebounce';
 
+
 export default function SearchPage() {
     
+    const navigate = useNavigate();
+
     const useQuery = () => {
         return new URLSearchParams(useLocation().search);
     }
@@ -40,8 +43,11 @@ export default function SearchPage() {
                     if(movie.backdrop_path !== null && movie.media_type !== "person"){
                         const movieImageUrl = "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
                         return (
-                            <div className="movie">
-                                <div className="movie__column-poster">
+                            <div className="movie" key={movie.id}>
+                                <div    
+                                    onClick={() => navigate(`/${movie.id}`)} 
+                                    className="movie__column-poster"
+                                >
                                     <img src={movieImageUrl} alt="" className="movie__poster" />
                                 </div>
                             </div>
